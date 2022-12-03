56d8bcdecb4432b053be1cccac5c381a497f8c2c
ElRafo92 <rafael.martinez@outlook.it>
2022-12-03 02:32:52 +0100

    creato input e button ricerca

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <AppHeader />
+</template>
+
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  export default {
+    nome: "App",
+    components: {
+      AppHeader,
+    },
+  }
+</script>
+
+<style lang="scss">
+@import "./style/global.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <input type="text" placeholder="Search a movie" v-model="store.SearchText">
+    <button>Search</button>
+</template>
+
+<script>
+    import {store} from "../store";
+    export default {
+        name: "AppHeader",
+        data() {
+            return {
+                store,
+            };
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
